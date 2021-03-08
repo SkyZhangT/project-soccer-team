d9ce8a704df849d20c099b8cb811898ca15434e1
SkyZhangT <36461750+SkyZhangT@users.noreply.github.com>
2021-03-07 16:15:44 -0800

    tweak nationality and rating

--- utils.py
+++ utils.py
@@ -13,34 +13,39 @@ class data_generator:
          'La Liga': ['Real Madrid', 'Barcelona', 'Atlético Madrid', 'Athletic Bilbao', 'Valencia'],
          'Serie A': ['Atalanta', 'Benevento', 'Bologna', 'Cagliari', 'Crotone'],
          'Ligue 1': ['Angers', 'Bordeaux', 'Brest', 'Dijon', 'Lens']}
-    nationality = ['Argentina', 'Belgium', 'Brazil', 'England', 'Spain']
+    league_nation = {'ANKARA': 'Turkey', 
+                    'Premier League': 'England',
+                    'La Liga': 'Spain',
+                    'Serie A': 'Italy',
+                    'Ligue 1': 'France'}
     data = list()
     
-    def __init__(self, size: int, nationality: list = None, teams: dict = None, outfile: str = 'players.json'):
+    def __init__(self, size: int, league_nation: list = None, teams: dict = None, outfile: str = 'players.json'):
         """
         Initialization can takes up to 4 arguments, three of them can be empty and defaulted to the ones above
 
         args:
-        size: an integer size of the dataset desired
+        size: an integer of desired size of the dataset
         nationality: a list of nations, can be any length
         teams: a dictionaty of league and teams, E.g.: {'league1': [team1, team2, team3]}
         outfile: str, name of the output file
         """
         self.outfile = outfile
-        if nationality != None:
-            self.nationality = nationality
+        if league_nation != None:
+            self.league_nation = league_nation
 
         if teams != None:
             self.teams = teams
 
         self.leagues = [key for key in self.teams.keys()]
+        self.nationality = [self.league_nation[key] for key in self.leagues]
         self.outfile = outfile
         self.size = size
         self.generate()
 
     def generate(self) -> list:
         """
-        generate can be called by outside function to generate multiple different datasets based on same
+        generate can be called by outside callers to generate multiple different datasets based on same
         league, team, nation information and distribution
         """
         self.clear_data()
@@ -54,10 +59,15 @@ class data_generator:
         """
         player = collections.defaultdict()
         player['name'] = fake.unique.name()
-        player['na'] = self.nationality[randint(0, len(self.nationality)-1)]
         player['league'] = self.leagues[randint(0, len(self.leagues)-1)]
         player['team'] = self.teams[player['league']][randint(0, len(self.teams[player['league']])-1)]
-        player['rating'] = randint(0, 100)
+
+        if randint(1, 10) <= 5:
+            player['na'] = self.league_nation[player['league']]
+        else:
+            player['na'] = self.nationality[randint(0, len(self.nationality)-1)]
+
+        player['rating'] = randint(70, 100)
         player['pos'] = self.position[randint(0, len(self.position)-1)]
         return player
 
